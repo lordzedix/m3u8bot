d57dc22bc0e087f43dd27b3dab35ba66ae7a30a9
lambda-stock <58011517+lambda-stock@users.noreply.github.com>
2021-09-20 21:21:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,9 @@ app = Client('m3u8', api_id, api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command('start'))
 async def start(_, message):
-    await message.reply('kullanım: `/convert m3u8_link`')
+    await message.reply(f'''Kullanım: `/convert m3u8_link`
+Github Repo: [Click to go.](https://github.com/lambda-stock/m3u8bot/)
+''')
 
 @app.on_message(filters.command(['convert', 'cevir']))
 async def convert(client, message):
@@ -21,33 +23,35 @@ async def convert(client, message):
         link = message.text.split(' ', 1)[1]
     except:
         print_exc()
-        return await message.reply('kullanım: `/convert m3u8_link`')
-    _info = await message.reply('bekle')
+        return await message.reply(f'''Kullanım: `/convert m3u8_link`
+Github Repo: [Click to go.](https://github.com/lambda-stock/m3u8bot/)
+''')
+    _info = await message.reply('Lütfen bekleyin...')
     filename = f'{message.from_user.id}_{int(time())}'
     proc = await asyncio.create_subprocess_shell(
         f'ffmpeg -i {link} -c copy -bsf:a aac_adtstoasc {filename}.mp4',
         stdout=PIPE,
         stderr=PIPE
     )
-    await _info.edit('bekle mp4 çeviriyom')
+    await _info.edit("Dosya mp4'e çevriliyor...")
     out, err = await proc.communicate()
-    await _info.edit('çevirdik')
+    await _info.edit('Dosya başarıyla çevrildi.')
     print('\n\n\n', out, err, sep='\n')
     try: 
-        await _info.edit('thumbnail çekiyom')
+        await _info.edit('Thumbnail ekleniyor...')
         proc2 = await asyncio.create_subprocess_shell(
             f'ffmpeg -i {filename}.mp4 -ss 00:00:00.000 -vframes 1 {filename}.jpg',
             stdout=PIPE,
             stderr=PIPE
         )
         await proc2.communicate()
-        await _info.edit('yüklüyom telegrama')
+        await _info.edit("Dosya Telegram'a yükleniyor...")
         def progress(current, total):
             print(message.from_user.first_name, ' -> ', current, '/', total, sep='')
         return await client.send_video(message.chat.id, f'{filename}.mp4', thumb=f'{filename}.jpg', progress=progress)
     except:
         print_exc()
-        return await _info.edit('sıçtı knk')
+        return await _info.edit('`Bir hata oluştu.`')
 
 
 app.run()
