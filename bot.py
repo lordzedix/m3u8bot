76e68d654067d0ab966578594b7676ece6b280f8
Zedix <91089397+lordzedix@users.noreply.github.com>
2022-03-29 09:28:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ Admin : [Click to go.](https://t.me/lordzedix)
     _info = await message.reply('Lütfen bekleyin...')
     
     proc = await asyncio.create_subprocess_shell(
-        ​f'ffmpeg -i "{link}" -c copy -bsf:a aac_adtstoasc ​{​filename​}​.mp4'​,
+       f'ffmpeg -i "{link}" -c copy -bsf:a aac_adtstoasc {filename}.mp4',
         stdout=PIPE,
         stderr=PIPE
     )
